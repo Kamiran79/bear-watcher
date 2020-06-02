a490165ba92542d14978e6e7c8b4ed0a12f58199
Kamiran Ibrahim <kamiran.ibrahim@gmail.com>
2020-06-01 21:27:36 -0500

    more

--- js/components/BestBear.js
+++ js/components/BestBear.js
@@ -3,7 +3,58 @@
 import bearData from "../helpers/data/bearData.js";
 import utils from "../helpers/utils.js";
 //import ControlEvent from "./ControlEvent.js";
-import bearDisplay from "./bearDisplay.js";
+//import bearDisplay from "./bearDisplay.js";
+
+
+const buildBestBearCard = () => {
+
+    //let myCurrentBestBearFind = false;
+    //let myLastBestBearFind = false;
+    const bearInfo = bearData.getBear();
+
+    const myIndex = (element) => element.currentRewards === 'Gold';
+    const myLastBestBear = (element) => element.lastRewards === 'Gold';
+    let getBestBear = bearInfo.findIndex(myIndex);
+    if (getBestBear === -1) {
+        getBestBear = bearInfo.findIndex(myLastBestBear);
+    }
+
+    let domString = '';
+    domString = `<h2> Best Bear </h2>`;
+    utils.printToDom("#currentDisplay", domString);
+    for (let i = 0; i < bearInfo.length; i++) {        
+        if (i === getBestBear){
+            console.log("inside the if statment to build domString");
+            domString =`                
+                    <div id="${bearInfo[i].id}" class="card mb-4 bear-card" >
+                        <img src="${bearInfo[i].imageUrl}" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">${bearInfo[i].name}</h5>   
+                            <ul class="list-group list-group-flush">
+                                <li class="list-group-item">Current Total Fish catchs: ${bearInfo[i].totalFish}</li>
+                                <li class="list-group-item">Duration: ${bearInfo[i].duration}</li>
+                                <li class="list-group-item">Current Rewards: ${bearInfo[i].currentRewards}</li>
+                                <li class="list-group-item">Last Rewards: ${bearInfo[i].lastRewards}</li>
+                            </ul>                                         
+                        </div>
+                    </div>
+                
+            `;            
+            //return;
+        } 
+    }
+
+    console.log(bearInfo);
+    //console.log(domString);
+    utils.printToDom("#reiverSide", domString);
+
+}
+
+
+export default { buildBestBearCard };
+
+
+/*move the top donwn here ..
 
 //ControlEvent.attachEvents();
 
@@ -11,7 +62,9 @@ function getAllBear1 (clicked) {
     bearDisplay.buildBearCard();
 }
 
-const buildBestBearCard = () => {
+
+
+const buildBestBearCard2 = () => {
     //const bearInfo = bearData.getBear();
     //$('#getAllBear').on('click')(bearDisplay.buildBearCard('hide'));
     //ControlEvent.attachEvents();
@@ -62,7 +115,10 @@ const buildBestBearCard = () => {
     //$('#get')
 }
 
-export default { buildBestBearCard };
+
+
+*/
+
 
 
 /*
--- js/components/bearDisplay.js
+++ js/components/bearDisplay.js
@@ -1,5 +1,6 @@
 import bearData from "../helpers/data/bearData.js"
 import utils from "../helpers/utils.js";
+import BestBear from "./BestBear.js";
 
 
 
@@ -7,6 +8,8 @@ import utils from "../helpers/utils.js";
 
 const myBear = bearData.getBear();
 const findDinoIndexById = (bearID) => mybear.findIndex(bear.id === bearID);
+let fishCatch = 0;
+
 
 const updateCard = (e) => {
     //console.log("fire event");
@@ -24,7 +27,8 @@ const updateCard = (e) => {
     //console.log(myBear.findIndex(n === myBear.id));
     //console.log("this is the index of bear", bearIndex);
     //e.preventDefault();
-    let fish = Math.floor(Math.random() * Math.floor(2));    
+    let fish = Math.floor(Math.random() * Math.floor(6));
+    fishCatch = fish;    
     //console.log("catch fish", fish);
     for (let i = 0; i<myBear.length; i++) {
         //const bear = myBear[i];
@@ -32,11 +36,13 @@ const updateCard = (e) => {
         //console.log("this is the index of bear", findDinoIndexById);
         //console.log(Number(myBear[i].id));
         //console.log(bearID);
+        myBear[i].currentCatch = 0;
         if (myBear[i].id === Number(bearID)) {
             //console.log("correct");
             myBear[i].totalFish +=fish;
-            myBear[i].duration +=0.5;
+            myBear[i].duration +=1;
             myBear[i].fat = myBear[i].totalFish/myBear[i].duration;
+            myBear[i].currentCatch = fish;
             //console.log("the current fish total", myBear[i].totalFish);
             //console.log("got the correct bear");
         }
@@ -67,26 +73,27 @@ const genericEventAttacher = (selector, functionToAttach) => {
 
 const buildBearCard = () => {
     const bearInfo = bearData.getBear();
-    const bestBear = bearData.getSelectedBestBear();
+    //const bestBear = bearData.getSelectedBestBear();
+    //console.log(bears);
     let domString = '';
     for (let i = 0; i < bearInfo.length; i++) {
         domString +=`
             <div class ="col-sm-4">
                 <div id="${bearInfo[i].id}" class="card mb-4 bear-card" style="width: 20rem;">
-                    <img src="${bearInfo[i].imageUrl}" class="card-img-top" alt="...">
+                    <img src="${bearInfo[i].imageUrl}" class="card-img-top card-img-top1" alt="...">
                     <div class="card-body">
                         <h5 class="card-title">${bearInfo[i].name}</h5>   
                         <ul class="list-group list-group-flush">
                             <li class="list-group-item">Current Total Fish catchs: ${bearInfo[i].totalFish}</li>
-                            <li class="list-group-item">Duration: ${bearInfo[i].duration}</li>
+                            <li class="list-group-item">Duration: ${bearInfo[i].duration} hrs.</li>
                             <li class="list-group-item">Fat: ${bearInfo[i].fat.toFixed(3)}</li>
                             <li class="list-group-item">Current Rewards: ${bearInfo[i].currentRewards}</li>
                             <li class="list-group-item">Last Rewards: ${bearInfo[i].lastRewards}</li>
                         </ul>                                         
                     </div>
                     <div class="card-body">                        
-                        <button type="button" class="btn btn-secondary getFish">Catch Fish</button>
-                        <a href="#" class="btn btn-primary">img show catch or not</a>
+                        <button type="button" class="btn btn-primary getFish">Catch Fish</button>                        
+                        <a href="#" class="btn btn-secondary">Fish Catch: ${bearInfo[i].currentCatch}</a>
                     </div>
                 </div>
             </div>
@@ -95,7 +102,7 @@ const buildBearCard = () => {
     console.log(bearInfo);
     //console.log(domString);
     utils.printToDom("#reiverSide", domString);
-    $('.bear-card').click(updateCard);
+    $('.getFish').click(updateCard);
     $('.getNewRewards').click(newRewards);
     //attachEvents();
     //document.querySelector("#getFish").addEventListener('click', updateCard);
@@ -104,11 +111,88 @@ const buildBearCard = () => {
 
 const newRewards = (e) => {
     e.preventDefault();
-    let i = Math.max.apply(Math, myBear.map(function(myBear) { return myBear.fat; }));
+    let goldFat = Math.max.apply(Math, myBear.map(function(myBear) { return myBear.fat; }));
+    let hm = myBear.filter(best => best.fat === i );
+    console.log("this is bounus filter to find how many fat there: ", hm.length);
+    //for (i = 0; i < myBear.length)
     //let i = Math.max(...myBear.fat);
-    console.log("this is the index of the best bear", i);
+    console.log("this is the index of the best bear", goldFat);
     console.log("this is new rewards");
 }
 
 
-export default { buildBearCard };
+
+const buildBestBearCard = () => {
+
+    BestBear.buildBestBearCard();
+
+    $('#getAllBear').click(buildBearCard);
+    $('.getNewRewards').click(newRewards);
+
+}
+
+
+
+
+export default { buildBearCard, buildBestBearCard };
+
+
+
+/*
+
+const buildBestBearCard = () => {
+    //const bearInfo = bearData.getBear();
+    //$('#getAllBear').on('click')(bearDisplay.buildBearCard('hide'));
+    //ControlEvent.attachEvents();
+    let myCurrentBestBearFind = false;
+    let myLastBestBearFind = false;
+    const bearInfo = bearData.getBear();
+    //const getBear = bearInfo.findIndex(bearInfo.currentRewards === 'Gold');
+
+    const myIndex = (element) => element.currentRewards === 'Gold';
+    const myLastBestBear = (element) => element.lastRewards === 'Gold';
+    let getBestBear = bearInfo.findIndex(myIndex);
+    if (getBestBear === -1) {
+        getBestBear = bearInfo.findIndex(myLastBestBear);
+    }
+
+    let domString = '';
+    domString = `<h2> Best Bear </h2>`;
+    utils.printToDom("#currentDisplay", domString);
+    for (let i = 0; i < bearInfo.length; i++) {        
+        if (i === getBestBear){
+            console.log("inside the if statment to build domString");
+            domString =`
+                
+                    <div id="${bearInfo[i].id}" class="card mb-4 bear-card" >
+                        <img src="${bearInfo[i].imageUrl}" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">${bearInfo[i].name}</h5>   
+                            <ul class="list-group list-group-flush">
+                                <li class="list-group-item">Current Total Fish catchs: ${bearInfo[i].totalFish}</li>
+                                <li class="list-group-item">Duration: ${bearInfo[i].duration}</li>
+                                <li class="list-group-item">Current Rewards: ${bearInfo[i].currentRewards}</li>
+                                <li class="list-group-item">Last Rewards: ${bearInfo[i].lastRewards}</li>
+                            </ul>                                         
+                        </div>
+                    </div>
+                
+            `;            
+            //return;
+        } 
+    }
+
+    console.log(bearInfo);
+    //console.log(domString);
+    utils.printToDom("#reiverSide", domString);
+    $('#getAllBear').click(buildBearCard);
+    $('.getNewRewards').click(newRewards);
+    
+    //$('getAllBear').click(ControlEvent.attachEvents());
+    //ControlEvent.attachEvents();
+    //document.querySelector("#getFish").addEventListener('click', updateCard);
+    //$('#get')
+}
+
+
+*/
